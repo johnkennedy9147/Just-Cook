bf7c3af8ec056db5b4c9fbe5b2f47ab3ce82c8bd
John Kennedy <jk@DESKTOP-CKMGIOD>
2023-09-21 10:42:33 +0100

    add meal selector

--- src/app.js
+++ src/app.js
@@ -4,13 +4,37 @@ import { DisplayRecipe } from './displayRecipe';
 import { useState } from 'react';
 
 export const App = () => {
-  // temporarily using a meal id I know exists for dev - remove later
+  // temporarily using meal id(s) I know exists for dev - remove later
   const [selectedRecipeId, setSelectedRecipeId] = useState(52772);
+  const sampleMealResult = [
+    {
+      strMeal: 'Chick-Fil-A Sandwich',
+      strMealThumb:
+        'https://www.themealdb.com/images/media/meals/sbx7n71587673021.jpg',
+      idMeal: '53016',
+    },
+    {
+      strMeal: 'Chicken Couscous',
+      strMealThumb:
+        'https://www.themealdb.com/images/media/meals/qxytrx1511304021.jpg',
+      idMeal: '52850',
+    },
+    {
+      strMeal: 'Chicken Fajita Mac and Cheese',
+      strMealThumb:
+        'https://www.themealdb.com/images/media/meals/qrqywr1503066605.jpg',
+      idMeal: '52818',
+    },
+  ];
+  const [recipeList, setRecipeList] = useState(sampleMealResult);
 
   return (
     <>
       <Form />
-      <RecipeList />
+      <RecipeList
+        recipeList={recipeList}
+        setSelectedRecipeId={setSelectedRecipeId}
+      />
       <DisplayRecipe selectedRecipeId={selectedRecipeId} />
     </>
   );
--- src/recipeList.js
+++ src/recipeList.js
@@ -1,3 +1,24 @@
-export const RecipeList = () => {
-  return <p>recipeList placeholder</p>;
+export const RecipeList = ({ recipeList, setSelectedRecipeId }) => {
+  return (
+    <article className='mb-3'>
+      <label htmlFor='mealSelector'>
+        <h4 className='text-md font-bold'>Select a Meal:</h4>
+      </label>
+      <select
+        id='mealSelector'
+        onChange={(e) => setSelectedRecipeId(e.target.value)}
+      >
+        {recipeList.length > 0 ? (
+          <option value=''>Select a Meal</option>
+        ) : (
+          <option value=''>Add some ingredients...</option>
+        )}
+        {recipeList.map(({ idMeal, strMeal }) => (
+          <option key={idMeal} value={idMeal}>
+            {strMeal}
+          </option>
+        ))}
+      </select>
+    </article>
+  );
 };
